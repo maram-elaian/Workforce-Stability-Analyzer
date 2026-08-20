7a2a9755dc1de983c981fda795eb0a5d62f7bc2b
Sad3nwl <sadeenabdelalrahman2@gmail.com>
2026-08-20 18:15:46 +0300

    data

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 df=pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
-# first 5 rows of the data
-print(df.head(5))
+# first 10 rows of the data
+print(df.head(10))
 print(f"Shape :{df.shape}")
 print("INFO ABOUT DATA:\n",df.info)
 print(df.describe())
@@ -18,5 +20,19 @@ print("Duplicate Rows:")
 print(df.duplicated().sum())
 # ========================DELETE COLUMN
 print(df.nunique().sort_values()) # BASED  on this we drop column
-
-
+col=[ "EmployeeCount","Over18","StandardHours","EmployeeNumber"]
+df.drop(columns=col, inplace=True)
+print(df.shape)
+# ================================data types
+print("Data Types:")
+print(df.dtypes)
+# ==================
+print(df["Attrition"].unique())
+print(df["Gender"].unique())
+print(df["Department"].unique())
+print(df["JobRole"].unique())
+print(df["OverTime"].unique())
+print(df["BusinessTravel"].unique())
+# ==================================outliers
+sns.boxplot(x=df["MonthlyIncome"])
+plt.show()
