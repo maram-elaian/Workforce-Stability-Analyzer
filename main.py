b06087b95c44fc342310e49ac8efa00fa2f504ea
Sad3nwl <sadeenabdelalrahman2@gmail.com>
2026-08-20 17:25:43 +0300

    data

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ import pandas as pd
 df=pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
 # first 5 rows of the data
 print(df.head(5))
-#
+print(f"Shape :{df.shape}")
+print("INFO ABOUT DATA:\n",df.info)
+print(df.describe())
+# =====================================================
+# NAMES OF THE COLUMNS
+print(df.columns)
+# ====================================================MISSING VALUES
+print("Missing Values:")
+print(df.isnull().sum())
+# total  of Missing values in data
+print("Total Missing Values:", df.isnull().sum().sum())
+# ====================================================DUPLICATE ROWS
+print("Duplicate Rows:")
+print(df.duplicated().sum())
