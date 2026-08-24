6ce03a40e0c775aab86d7619331b02eb8d96d57d
Sad3nwl <sadeenabdelalrahman2@gmail.com>
2026-08-24 23:45:52 +0300

    EDA

--- EDA.py
+++ EDA.py
@@ -45,3 +45,45 @@ plt.title("Employee Attrition Distribution")
 plt.xlabel("Attrition")
 plt.ylabel("Number of Employees")
 plt.show()
+"""Which age group is most represented?
+
+Is the data normally distributed?
+
+Are there too few or too many age groups?"""
+print(df["Age"].describe())
+plt.figure(figsize=(8, 5))
+sns.histplot(
+    data=df,
+    x="Age",
+    bins=20,
+    kde=True
+)
+plt.title("Age Distribution of Employees")
+plt.xlabel("Age")
+plt.ylabel("Number of Employees")
+plt.tight_layout()
+plt.show()
+# We want to know the distribution of employees by gender.
+print(df["Gender"].value_counts())
+plt.figure(figsize=(7, 5))
+ax = sns.countplot(
+    data=df,
+    x="Gender"
+)
+plt.title("Employee Distribution by Gender")
+plt.xlabel("Gender")
+plt.ylabel("Number of Employees")
+plt.tight_layout()
+plt.show()
+# for the department
+print(df["Department"].value_counts())
+plt.figure(figsize=(8, 5))
+dx = sns.countplot(
+    data=df,
+    x="Department"
+)
+plt.title("Employee Distribution by Department")
+plt.xlabel("Department")
+plt.ylabel("Number of Employees")
+plt.tight_layout()
+plt.show()
