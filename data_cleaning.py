633d7afb02344529e30b7260564847e1573d377f
Sad3nwl <sadeenabdelalrahman2@gmail.com>
2026-08-24 23:00:15 +0300

    data

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-df=pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+df=pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition_.csv")
 # first 10 rows of the data
 print(df.head(10))
 print(f"Shape :{df.shape}")
@@ -36,5 +36,5 @@ print(df["BusinessTravel"].unique())
 # ==================================outliers
 sns.boxplot(x=df["MonthlyIncome"])
 plt.show()
-df.to_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv", index=False)
+df.to_csv("WA_Fn-UseC_-HR-Employee-Attrition_clean.csv", index=False)
 
