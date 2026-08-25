036ac48a9e7c309b182fb5b898949842624b4a72
Sad3nwl <sadeenabdelalrahman2@gmail.com>
2026-08-25 23:44:03 +0300

    EDA

--- EDA.py
+++ EDA.py
@@ -1,115 +1,218 @@
 import pandas as pd
-import  numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-df=pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition_clean.csv")
-# ==============================Dataset Overview===========================
+import matplotlib.pyplot as plt
+df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition_clean.csv")
+# ============================== Dataset Overview ============================
 print(df.shape)
 print(df.head())
-print(df.info())
+df.info()
 print(df.describe())
-# ===================Attrition KPI==============
+# ============================== Attrition KPI ===============================
 # Total Employees
-Totalemployee=len(df)
-print(f"the Total number of employee:{Totalemployee}")
+Totalemployee = len(df)
+print(f"the Total number of employee: {Totalemployee}")
 # Employees Who Left
-Employee_left=(df["Attrition"] == "Yes").sum()
-print(f"number of employee who left :{Employee_left}")
+Employee_left = (df["Attrition"] == "Yes").sum()
+print(f"number of employee who left: {Employee_left}")
 # Employees Who Stayed
-Employee_stay= (df["Attrition"] == "No").sum()
-print(f"number of employee who stays :{Employee_stay}")
+Employee_stay = (df["Attrition"] == "No").sum()
+print(f"number of employee who stays: {Employee_stay}")
 # Attrition Rate
-attrition_rate=(Employee_left/Totalemployee)*100
-print(f"Attrition Rate:{attrition_rate}")
+attrition_rate = (Employee_left / Totalemployee) * 100
+print(f"Attrition Rate: {attrition_rate:.2f}%")
+# KPI Table
 kpi = pd.DataFrame({
-    "Q": [
-        "Total Employees",
+    "Q": ["Total Employees",
         "Employees Left",
         "Employees Stayed",
-        "Attrition Rate"
-    ],
-    "Value": [
-        Totalemployee,
+        "Attrition Rate"],
+    "Value":[Totalemployee,
         Employee_left,
         Employee_stay,
-        f"{attrition_rate:.2f}%"
-    ]
-})
-# How many employee left the company compared with those who stayed?
-plt.figure(figsize=(5,7))
+        f"{attrition_rate:.2f}%"]})
+print(kpi)
+# ============================================================
+#ATTRITION DISTRIBUTION
+# We want to know how many employees left and stayed.
+plt.figure(figsize=(6, 5))
 sns.countplot(
     data=df,
-    x="Attrition"
-)
+    x="Attrition")
 plt.title("Employee Attrition Distribution")
 plt.xlabel("Attrition")
 plt.ylabel("Number of Employees")
-plt.show()
-"""Which age group is most represented?
-
-Is the data normally distributed?
-
-Are there too few or too many age groups?"""
-print(df["Age"].describe())
-plt.figure(figsize=(8, 5))
-sns.histplot(
-    data=df,
-    x="Age",
-    bins=20,
-    kde=True
-)
-plt.title("Age Distribution of Employees")
-plt.xlabel("Age")
-plt.ylabel("Number of Employees")
 plt.tight_layout()
 plt.show()
-# We want to know the distribution of employees by gender.
-print(df["Gender"].value_counts())
+# ============================================================
+#ATTRITION BY OVERTIME
+# We want to know if overtime is related to employee attrition.
+overtime_attrition = pd.crosstab(
+    df["OverTime"],
+    df["Attrition"],
+    normalize="index") * 100
+print(overtime_attrition)
 plt.figure(figsize=(7, 5))
-ax = sns.countplot(
-    data=df,
-    x="Gender"
-)
-plt.title("Employee Distribution by Gender")
-plt.xlabel("Gender")
-plt.ylabel("Number of Employees")
+sns.barplot(
+    data=overtime_attrition.reset_index(),
+    x="OverTime",
+    y="Yes")
+plt.title("Attrition Rate by Overtime")
+plt.xlabel("OverTime")
+plt.ylabel("Attrition Rate (%)")
 plt.tight_layout()
 plt.show()
-# for the department
-print(df["Department"].value_counts())
-plt.figure(figsize=(8, 5))
-dx = sns.countplot(
-    data=df,
-    x="Department"
-)
-plt.title("Employee Distribution by Department")
-plt.xlabel("Department")
-plt.ylabel("Number of Employees")
-plt.tight_layout()
-plt.show()
-# JOB RULE
-print(df["JobRole"].value_counts())
+# ============================================================
+#ATTRITION BY JOB ROLE
+# We want to know which job roles have a higher attrition rate.
+jobrole_attrition = pd.crosstab(
+    df["JobRole"],
+    df["Attrition"],
+    normalize="index") * 100
+print(jobrole_attrition)
 plt.figure(figsize=(10, 6))
-JRx = sns.countplot(
-    data=df,
-    y="JobRole",
-    order=df["JobRole"].value_counts().index
-)
-plt.title("Employee Distribution by Job Role")
-plt.xlabel("Number of Employees")
+sns.barplot(
+    data=jobrole_attrition.reset_index(),
+    x="Yes",
+    y="JobRole")
+plt.title("Attrition Rate by Job Role")
+plt.xlabel("Attrition Rate (%)")
 plt.ylabel("Job Role")
 plt.tight_layout()
 plt.show()
-print(df["MonthlyIncome"].describe())
+# ============================================================
+#ATTRITION BY AGE GROUP
+# We want to know which age group has a higher attrition rate.
+df["AgeGroup"] = pd.cut(
+    df["Age"],
+    bins=[17, 25, 35, 45, 55, 65],
+    labels=["18-25", "26-35", "36-45", "46-55", "56-65"])
+age_attrition = pd.crosstab(
+    df["AgeGroup"],
+    df["Attrition"],
+    normalize="index") * 100
+print(age_attrition)
 plt.figure(figsize=(8, 5))
-sns.histplot(
-    data=df,
-    x="MonthlyIncome",
-    bins=30,
-    kde=True
-)
-plt.title("Monthly Income Distribution")
-plt.xlabel("Monthly Income")
-plt.ylabel("Number of Employees")
+sns.barplot(
+    data=age_attrition.reset_index(),
+    x="AgeGroup",
+    y="Yes")
+plt.title("Attrition Rate by Age Group")
+plt.xlabel("Age Group")
+plt.ylabel("Attrition Rate (%)")
 plt.tight_layout()
 plt.show()
+# ============================================================
+#MONTHLY INCOME BY ATTRITION
+# We want to compare the income of employees who left and stayed.
+plt.figure(figsize=(8, 5))
+sns.boxplot(
+    data=df,
+    x="Attrition",
+    y="MonthlyIncome")
+plt.title("Monthly Income by Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Monthly Income")
+plt.tight_layout()
+plt.show()
+# ============================================================
+# JOB SATISFACTION BY ATTRITION
+# We want to know if job satisfaction is related to attrition.
+satisfaction_attrition = pd.crosstab(
+    df["JobSatisfaction"],
+    df["Attrition"],
+    normalize="index") * 100
+print(satisfaction_attrition)
+plt.figure(figsize=(8, 5))
+sns.barplot(
+    data=satisfaction_attrition.reset_index(),
+    x="JobSatisfaction",
+    y="Yes")
+plt.title("Attrition Rate by Job Satisfaction")
+plt.xlabel("Job Satisfaction Level")
+plt.ylabel("Attrition Rate (%)")
+plt.tight_layout()
+plt.show()
+# ============================================================
+#YEARS AT COMPANY BY ATTRITION
+# We want to compare years at company between employees who left and stayed.
+plt.figure(figsize=(8, 5))
+sns.boxplot(
+    data=df,
+    x="Attrition",
+    y="YearsAtCompany")
+plt.title("Years at Company by Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Years at Company")
+plt.tight_layout()
+plt.show()
+# ============================================================
+# DISTANCE FROM HOME BY ATTRITION
+# We want to compare the distance from home between employees who left and stayed.
+plt.figure(figsize=(8, 5))
+sns.boxplot(
+    data=df,
+    x="Attrition",
+    y="DistanceFromHome")
+plt.title("Distance From Home by Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Distance From Home")
+plt.tight_layout()
+plt.show()
+# ============================================================
+# BUSINESS INSIGHTS
+# We want to find the main factors related to employee attrition.
+# Overall Attrition Rate
+print("Overall Attrition Rate:")
+print(f"{attrition_rate:.2f}%")
+# ============================================================
+# Attrition Rate by Overtime
+print("\nAttrition Rate by Overtime:")
+overtime_rate = pd.crosstab(
+    df["OverTime"],
+    df["Attrition"],
+    normalize="index") * 100
+print(overtime_rate["Yes"].sort_values(ascending=False))
+# ============================================================
+#Attrition Rate by Job Role
+print("\nAttrition Rate by Job Role:")
+jobrole_rate = pd.crosstab(
+    df["JobRole"],
+    df["Attrition"],
+    normalize="index") * 100
+print(jobrole_rate["Yes"].sort_values(ascending=False))
+# ============================================================
+#Attrition Rate by Age Group
+print("\nAttrition Rate by Age Group:")
+age_rate = pd.crosstab(
+    df["AgeGroup"],
+    df["Attrition"],
+    normalize="index") * 100
+print(age_rate["Yes"].sort_values(ascending=False))
+# ============================================================
+#Attrition Rate by Job Satisfaction
+print("\nAttrition Rate by Job Satisfaction:")
+satisfaction_rate = pd.crosstab(
+    df["JobSatisfaction"],
+    df["Attrition"],
+    normalize="index") * 100
+print(satisfaction_rate["Yes"].sort_values(ascending=False))
+# ============================================================
+#Attrition Rate by Department
+print("\nAttrition Rate by Department:")
+department_rate = pd.crosstab(
+    df["Department"],
+    df["Attrition"],
+    normalize="index") * 100
+print(department_rate["Yes"].sort_values(ascending=False))
+# ============================================================
+#Average Years at Company by Attrition
+print("\nAverage Years at Company by Attrition:")
+print( df.groupby("Attrition")["YearsAtCompany"].mean())
+# ============================================================
+#Average Monthly Income by Attrition
+print("\nAverage Monthly Income by Attrition:")
+print(df.groupby("Attrition")["MonthlyIncome"].mean())
+# ============================================================
+# 9. Average Distance From Home by Attrition
+print("\nAverage Distance From Home by Attrition:")
+print(df.groupby("Attrition")["DistanceFromHome"].mean())
